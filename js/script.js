24dca15b525a171333803d08a2f47abdb39c33c1
NgocTrungBe <bengoctrung23@gmail.com>
2021-10-18 10:31:38 +0700

    trending product,nav

--- js/script.js
+++ js/script.js
@@ -5,7 +5,18 @@ const nice_sellect = $.getElementById("nice-select");
 const options = $.getElementById("options");
 const current = $.getElementsByClassName("current");
 
-// document.addEventListener("click", openSellect);
+// nav trending
+const nav_items = $.getElementById("nav_main").querySelectorAll("li");
+const item_active = $.getElementsByClassName("nav-item active");
+const tab_pane = $.getElementsByClassName("tab-pane");
+const tab_pane_active = $.getElementsByClassName("tab-pane active");
+const tab_pane_fade = $.getElementsByClassName("tab-pane fade");
+const tab_pane_show = $.getElementsByClassName("tab-pane show");
+
+
+
+console.log(tab_pane_active)
+    // document.addEventListener("click", openSellect);
 
 
 nice_sellect.addEventListener("click", function() {
@@ -18,3 +29,33 @@ options.addEventListener("click", function(event) {
     current[0].innerText = value;
 
 });
+
+
+nav_items.forEach((item, index) => {
+    item.onclick = function() {
+        item_active[0].classList.remove("active");
+        this.classList.add("active")
+
+
+
+
+        if (tab_pane_active.length != 0 && tab_pane_fade.length != 0 && tab_pane_show.length != 0) {
+            tab_pane_active[0].classList.remove("active");
+            tab_pane_fade[0].classList.remove("fade");
+            tab_pane_show[0].classList.remove("show");
+        }
+        if (tab_pane.length != 0 && tab_pane.length > index) {
+            tab_pane[index].classList.add("active");
+            tab_pane[index].classList.add("fade");
+            tab_pane[index].classList.add("show");
+        }
+
+
+
+
+
+
+
+
+    }
+})
